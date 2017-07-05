ed6a6d4cb1651626460763dfb377f42ef4c6f866
Thomas Deppisch <thomas.deppisch@web.de>
2017-07-05 16:06:49 +0200

    hello_world.cpp: implemented switch for command line input

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-  cout << "\n    Hello world.\n\n";
+  
+  switch(argv[1][0]){
+  case '1' :
+    cout << "\n    Hello sun.\n\n";
+    break;
+  case '2' :
+    cout << "\n    Hello moon.\n\n";
+    break;
+  case '3':
+    cout << "\n    Hello stars.\n\n";
+    break;
+  default :
+    cout << "\n    Hello world.\n\n";
+  }
+  
 }
